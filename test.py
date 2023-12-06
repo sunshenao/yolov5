95804627feca409a1ca40fcec0acec407054d209
sunshenao <410339831@qq.com>
2023-12-06 10:50:54 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,16 @@
+import shutil
+import os
+
+# 源文件夹路径
+source_folder = 'utils'
+
+# 目标文件夹路径
+destination_folder = 'qwe'
+
+# 递归复制文件夹及其内容
+shutil.copytree(source_folder, destination_folder)
+
+
+
+
+
